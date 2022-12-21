53ccf8dc12c7a680f62b77d094f0aee5303906f0
Kenjiro Yamada <76738596+y-kethub@users.noreply.github.com>
2022-12-21 23:11:20 +0900

    Initial Commit

---
+++ City Guide/City_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_GuideApp.swift
+//  City Guide
+//
+//  Created by developer on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct City_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
